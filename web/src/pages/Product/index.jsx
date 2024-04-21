b35af608a51199b28d75b48c2cac7282ae34c781
iCaioReis <caioreis3186@gmail.com>
2024-04-20 23:39:38 -0300

    Delete user password on session route

--- web/src/pages/Product/index.jsx
+++ web/src/pages/Product/index.jsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import { useAuth } from '../../hooks/auth';
+import { USER_ROLE } from '../../utils/roles';
 
 import { Container, Header, Item } from "./styles";
 import { Button } from '../../components/Button';
 
 export function Product() {
+  const { user } = useAuth();
   const navigate = useNavigate();
 
   const products = Array(20)
@@ -16,7 +19,9 @@ export function Product() {
         <h1>Produtos</h1>
 
         <nav>
-          <Button title="Cadastrar" />
+          {
+            [user.role === USER_ROLE.ADMIN && <Button title="Cadastrar" />]
+          }
           <Button title="Voltar" onClick={() => navigate('/')} />
         </nav>
       </Header>
